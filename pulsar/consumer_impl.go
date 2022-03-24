32fee5fcf4d4d364555d972cba94229a58df2a4a
yougg <joe2qq@gmail.com>
2022-03-24 21:43:06 +0800

    log the message received from closed channel

--- pulsar/consumer_impl.go
+++ pulsar/consumer_impl.go
@@ -25,6 +25,7 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 
 	"github.com/apache/pulsar-client-go/pulsar/crypto"
 	"github.com/apache/pulsar-client-go/pulsar/internal"
@@ -438,6 +439,9 @@ func (c *consumer) Receive(ctx context.Context) (message Message, err error) {
 			return nil, newError(ConsumerClosed, "consumer closed")
 		case cm, ok := <-c.messageCh:
 			if !ok {
+				if cm.Message != nil && (*(*[2]uintptr)(unsafe.Pointer(&cm.Message)))[1] != 0 {
+					c.log.WithField("message", cm.Message).Warnf("get message from closed channel")
+				}
 				return nil, newError(ConsumerClosed, "consumer closed")
 			}
 			return cm.Message, nil
