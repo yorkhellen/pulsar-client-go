c863fa0a799b05fa6f1549e8273132f8f49e40f8
yougg <joe2qq@gmail.com>
2022-04-02 18:43:32 +0800

    Fix ack timeout cause reconnect sync from: https://github.com/apache/pulsar-client-go/pull/756/files

--- pulsar/producer_partition.go
+++ pulsar/producer_partition.go
@@ -786,10 +786,7 @@ func (p *partitionProducer) ReceivedSendReceipt(response *pb.CommandSendReceipt)
 
 	if !ok {
 		// if we receive a receipt although the pending queue is empty, the state of the broker and the producer differs.
-		// At that point, it is better to close the connection to the broker to reconnect to a broker hopping it solves
-		// the state discrepancy.
 		p.log.Warnf("Received ack for %v although the pending queue is empty, closing connection", response.GetMessageId())
-		p._getConn().Close()
 		return
 	}
 
